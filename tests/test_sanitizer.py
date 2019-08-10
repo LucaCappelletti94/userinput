d54529e7ff894cd1986a76e63aff28f865951c90
Luca Cappelletti <cappelletti.luca94@gmail.com>
2019-08-10 09:07:36 +0200

    Extended testing

---
+++ tests/test_sanitizer.py
@@ -0,0 +1,8 @@
+from userinput import userinput
+import pytest
+
+
+def test_input(monkeypatch):
+    user_input = "yes"
+    monkeypatch.setattr('builtins.input', lambda x: user_input)
+    assert userinput("user_input", validator="human_bool", sanitizer="human_bool")
