ff4bbdf85e8a0060f1845f948f33f17993c1e069
Luca Cappelletti <cappelletti.luca94@gmail.com>
2019-08-10 09:12:19 +0200

    Extended testing

--- tests/test_hidden.py
+++ tests/test_hidden.py
@@ -5,4 +5,4 @@ import pytest
 def test_hidden(monkeypatch):
     user_input = "yes"
     monkeypatch.setattr('getpass.getpass', lambda x: user_input)
-    assert userinput("user_input", validator="human_bool", sanitizer="human_bool", hidden=True)
+    assert userinput("user_input", validator="human_bool", sanitizer="human_bool", cache=False, hidden=True)
